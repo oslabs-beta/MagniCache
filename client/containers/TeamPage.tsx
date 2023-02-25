518e24ea4c47b31c225f358061cb99621d8c8671
ariasoltank <aasolt99@gmail.com>
2023-02-25 13:13:33 -0800

    set up react router links

---
+++ client/containers/TeamPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const TeamPage: React.FC = () => {
+  return (
+    <>
+      <h1>Our Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente esse
+        laudantium earum at. Quae obcaecati quidem rerum earum impedit. Delectus
+        magni quam explicabo, debitis sed iure dignissimos ratione eligendi
+        quod?
+      </p>
+    </>
+  );
+};
+
+export default TeamPage;
