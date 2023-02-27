8cad14e0b3a3495ffa85a38f10dedd498746d13c
Ahmed Chami <achami8799@gmail.com>
2023-02-27 16:13:57 -0500

    updates for merging queries, checks for mutations to be cached

--- magnicache-server/magnicache-server.ts
+++ magnicache-server/magnicache-server.ts
@@ -9,107 +9,149 @@ import { Response, Request, NextFunction } from 'express';
 
 import * as mergeWith from 'lodash/mergeWith';
 
+// Magnicache class for creating a queryable cache
 function Magnicache(this: any, schema: any): void {
+  // save the provided schema
   this.schema = schema;
+
+  // create a map for caching query responses
   this.cache = new Map();
+
+  // bind the query method for use in other functions
   this.query = this.query.bind(this);
 }
 
-function bypass(req: Request, res: Response, next: NextFunction): void {
-  const { query } = req.body;
-  graphql({ schema: this.schema, source: query })
-    .then((result: {}) => {
-      // console.log('qRes;', queryResponses);
-      // console.log('result;', result);
-      res.locals.queryResponse = result;
-      return next();
-    })
-    .catch((err: {}) => {
-      console.log(err);
-      return next({
-        log: err,
-      });
-    });
-}
-
+// Query method takes request, response and next callbacks
+// as its arguments
 Magnicache.prototype.query = function (
   req: Request,
   res: Response,
   next: NextFunction
 ): void {
+  // get the body of the requested query
   const { query } = req.body;
-  const AST = parse(query); // parse() gives us an abstract syntax tree
-  const { definitions } = AST;
-  const [ast] = definitions;
 
+  // parse the query into an AST
+  const { definitions: [ast] } = parse(query);
+
+  // check if the operation is a query
+  // and not some other type of mutation
   if (ast.operation === 'query') {
-    // console.log(ast.selectionSet.selections[0].name.value)
     if (ast.selectionSet.selections[0].name.value === '__schema') {
+      // execute the graphl query against the schema
+      graphql({ schema: this.schema, source: query })
+        .then((result: {}) => {
+          // assign the result to the response locals
+          res.locals.queryResponse = result;
 
-      graphql({ schema: this.schema, source: query }).then((result: {}) => {
-   
+          // proceed to execute the next callback
+          return next();
+        })
+        .catch((err: {}) => {
+          console.log(err);
+
+          // throw an error to the next callback
+          return next({
+            log: err,
+          });
+        });
+    } else {
+      // get the selection set
+      const queries: string[] = this.magniParser(ast.selectionSet.selections);
+
+      // store the query results
+      const queryResponses: {}[] = [];
+
+      // compile all individual query responses
+      const compileQueries = () => {
+        let response1: {} = {};
+
+        for (const queryResponse of queryResponses) {
+          response1 = mergeWith(response1, queryResponse);
+        }
+
+        // assign the combined result to the response locals
+        res.locals.queryResponse = response1;
+
+        console.log(this.cache);
+
+        // proceed to execute the next callback
+        return next();
+      };
+
+      // loop through the individual queries and execute them in turn
+      for (const query of queries) {
+        // check if query is already cached
+        if (this.cache.has(query)) {
+          // output message indicating that the query is cached
+          console.log('cache hit');
+
+          // store the cached response
+          queryResponses.push(this.cache.get(query));
+
+          // check if all queries have been fetched
+          if (queries.length === queryResponses.length) {
+            // if yes, compile all queries
+            compileQueries();
+          }
+        } else {
+          // output message indicating that the query is missing
+          console.log('cache miss');
+
+          // execute the query against graphql
+          graphql({ schema: this.schema, source: query })
+            .then((result: {}) => {
+              // cache the newest response
+              this.cache.set(query, result);
+
+              // store the query response
+              queryResponses.push(result);
+
+              // check if all queries have been fetched
+              if (queries.length === queryResponses.length) {
+                // if yes, compile all queries
+                compileQueries();
+              }
+            })
+            .catch((err: {}) => {
+              console.log(err);
+
+              // throw an error to the next callback
+              return next({
+                log: err,
+              });
+            });
+        }
+      }
+    }
+    // not a query!!
+  } else if (ast.operation === 'mutation') {
+    console.log('this is a mutation');
+    // Logic for mutation goes here
+    this.cache = new Map();
+    console.log(this.cache);
+    graphql({ schema: this.schema, source: query })
+      .then((result: {}) => {
         res.locals.queryResponse = result;
         return next();
+      })
+      .catch((err) => {
+        console.log(err);
+        return next({
+          log: err,
+        });
       });
-    }
-    const queries: string[] = this.magniParser(ast.selectionSet.selections);
-    // console.log('queries', queries);
-    const queryResponses: {}[] = [];
-
-    // this compileQueries function needs work -> currently it is not compiling all of our queries because each messageById value is an array?
-    const compileQueries = () => {
-      let response1: {} = {};
-
-      for (const queryResponse of queryResponses) {
-        // console.log(JSON.stringify(response));
-        response1 = mergeWith(response1, queryResponse);
-      }
-      /*[{"data":{"messageById":[{"message":"\n\nHey there everyone! I've been having such a great time getting to know all of you"}]}},{"data":{"messageById":[{"sender_id":1}]}}] */
-
-      res.locals.queryResponse = response1;
-      // console.log(this.cache);
-      return next();
-    };
-
-    for (const query of queries) {
-      if (this.cache.has(query)) {
-        // console.log('cache hit');
-        queryResponses.push(this.cache.get(query));
-        if (queries.length === queryResponses.length) {
-          // console.log(queryResponses);
-          compileQueries();
-        }
-      } else {
-        // console.log('cache miss');
-
-        graphql({ schema: this.schema, source: query })
-          .then((result: {}) => {
-            // console.log('qRes;', queryResponses);
-            // console.log('result;', result);
-
-            this.cache.set(query, result);
-
-            queryResponses.push(result);
-            if (queries.length === queryResponses.length) {
-              compileQueries();
-            }
-          })
-          .catch((err: {}) => {
-            console.log(err);
-            return next({
-              log: err,
-            });
-          });
-      }
-    }
-
-    // not a query!!
-  } else {
-    console.log('this is a mutation');
-    return next();
   }
+  /*addMessage: {
+        type: MessageType,
+        description: 'add a message to the db',
+        args: {
+          sender_id: { type: GraphQLInt },
+          message: { type: GraphQLString },
+        }, */
 };
 
+// Function that takes an array of selections and generates an array of strings based off of them
 Magnicache.prototype.magniParser = function (
   selections: {
     kind: string;
@@ -136,27 +178,31 @@ Magnicache.prototype.magniParser = function (
   queryArray: (string | string[])[] = [],
   queries: string[] = []
 ): string[] {
+  //Logging that the parser is running
   console.log('parsing');
+
+  // Looping through the selections to build the queries array
   for (const selection of selections) {
     queryArray.push(selection.name.value);
     if (selection.arguments?.length > 0) {
       const argumentArray: string[] = [];
 
+      // looping through the arguments to add them to the argument array
       for (const argument of selection.arguments) {
         argumentArray.push(`${argument.name.value}:${argument.value.value}`);
       }
       //['id:4','name:john']
       queryArray.push([argumentArray.join(',')]);
     }
+    // Checking for a selection set in the selection
     if (selection.selectionSet) {
       this.magniParser(selection.selectionSet.selections, queryArray, queries);
     } else {
       let string = ``;
       //{allMessages(id:4){message}}
-      // console.log('queryArray:', queryArray);
-
       // Ex:  ['messageById', ['id:4'], ['name:yousuf'], 'message']
       // would give {messageById(id:4,name:yousuf){message}}
+      // Looping through the query array to build the string
       for (let i = queryArray.length - 1; i >= 0; i--) {
         if (Array.isArray(queryArray[i])) {
           string = `(${queryArray[i][0]})${string}`;
@@ -164,11 +210,18 @@ Magnicache.prototype.magniParser = function (
           string = `{${queryArray[i] + string}}`;
         }
       }
+      // Adding the final string to the queries array
       queries.push(string);
     }
+    // Removing the element from the query array
     queryArray.pop();
   }
+  // Returning the queries array with all strings
   return queries;
 };
+
+Magnicache.prototype.removeEnd = () => {
+  
+}
 
 module.exports = Magnicache;
