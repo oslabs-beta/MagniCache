7fd8b308cc414084834ae8df9235b4015b13c587
Ahmed Chami <achami8799@gmail.com>
2023-03-04 12:03:21 -0500

    creates charts for certain cache metrics, display static data

--- client/components/CacheMetrics.tsx
+++ client/components/CacheMetrics.tsx
@@ -1,47 +1,85 @@
-import React, { SetStateAction } from 'react';
+import React, { SetStateAction, useEffect } from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+} from 'chart.js';
+import { Pie, Doughnut } from 'react-chartjs-2';
 
 interface CacheProps {
   usage: number | string;
-  size: string;
+  size: string | number;
   resTime: number;
-  avgCached: number;
-  avgUncached: number;
-  currentPolicy: string;
+  avgCached: number | string;
+  avgUncached: number | string;
 
   // possibly need state update doubt it but adding just in case
-    // setUsage: React.Dispatch<SetStateAction<string | number>>
-    // setSize: React.Dispatch<SetStateAction<number>>
-    // setResTime: React.Dispatch<SetStateAction<number>>
-    // setCached: React.Dispatch<SetStateAction<number>>
-    // setUncached: React.Dispatch<SetStateAction<number>>
-    // setPolicy: React.Dispatch<SetStateAction<string>>
+  // setUsage: React.Dispatch<SetStateAction<string | number>>
+  // setSize: React.Dispatch<SetStateAction<number>>
+  // setResTime: React.Dispatch<SetStateAction<number>>
+  // setCached: React.Dispatch<SetStateAction<number>>
+  // setUncached: React.Dispatch<SetStateAction<number>>
 }
 
 const CacheMetrics = (props: CacheProps) => {
-  const { usage, size, resTime, avgCached, avgUncached, currentPolicy } = props;
+  const { usage, size, resTime, avgCached, avgUncached } = props;
+
+  const usageData = {
+    labels: ['Space Used', 'Space Left'],
+    datasets: [
+      {
+        labels: ['Space Used', 'Space Left'],
+        data: [usage, size],
+        backgroundColor: ['#5b2af0', '#00CC99'],
+        borderColor: ['white'],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const avgData = {
+    labels: ['Avg. Cached in ms', 'Avg. Uncached in ms'],
+    datasets: [
+      {
+        data: [avgCached, avgUncached],
+        backgroundColor: ['#5b2af0', '#b3001b'],
+        borderColor: ['white'],
+        borderWidth: 1,
+      },
+    ],
+  };
+
   return (
     <div className="cache-metric-container">
-      <div className="cache-metric">Cache Usage: {usage}</div>
+      <div className="cache-metric">Cache Capacity Used: {usage}</div>
       <br></br>
 
-      <div className="cache-metric">Cache Size: {size} </div>
+      <div className="cache-metric">Remaining Capacity: {size} </div>
+      <br></br>
+
+      <div className="cache-metric">Total Average Response Time: {resTime}</div>
       <br></br>
 
       <div className="cache-metric">
-        Total Average Response Time: {resTime}{' '}
+        Average Cached Response Time: {avgCached}
       </div>
       <br></br>
 
       <div className="cache-metric">
-        Average Cached Response Time: {avgCached}{' '}
-      </div>
-      <br></br>
-
-      <div className="cache-metric">
-        Average Uncached Response Time: {avgUncached}{' '}
+        Average Uncached Response Time: {avgUncached}
       </div>
       <br></br>
       {/* Possibly add a chart of sorts here? Maybe 2 or 3?  Line graph? bar? Pie?  */}
+      <div className='chart-container'> 
+      <Doughnut  data={usageData} style={{position: 'absolute', left: '50%', right: '50%', marginRight:'1%'}}/>
+      <Pie  data={avgData} style={{position: 'absolute', right: 0}}/>
+      </div>
     </div>
   );
 };
--- client/containers/XMetrics.tsx
+++ client/containers/XMetrics.tsx
@@ -2,15 +2,19 @@ import React, { useState, useEffect } from 'react';
 import CacheMetrics from '../components/CacheMetrics';
 
 const XMetrics = () => {
-  const [usage, setUsage] = useState<number | string>('');
-  const [size, setSize] = useState<string>('');
-  const [resTime, setResTime] = useState<number>(0);
-  const [avgCached, setCached] = useState<number>(0);
-  const [avgUncached, setUncached] = useState<number>(0);
-  const [currentPolicy, setPolicy] = useState<string>('');
+  const [usage, setUsage] = useState<number | string>(84);
+  const [size, setSize] = useState<string | number>(16);
+  const [resTime, setResTime] = useState<number>(275.3);
+  const [avgCached, setCached] = useState<number | string>(31.3);
+  const [avgUncached, setUncached] = useState<number | string>(520.4);
 
   // Send a request and populate proper values to be passed down to CacheMetrics
-  fetch('some endpoint')
+  fetch('/some endpoint')
+    .then((res) => res.json())
+    .then((data) => data);
+    // usage will be a number possibly converted to Bytes, same with size
+    // ResTime will be the total average response time of uncached and cached
+    // AvgCached and AvgUncached will be the averages for each result
 
   return (
     <div className="extra-metrics-container">
@@ -21,7 +25,6 @@ const XMetrics = () => {
         resTime={resTime}
         avgCached={avgCached}
         avgUncached={avgUncached}
-        currentPolicy={currentPolicy}
       />
     </div>
   );
